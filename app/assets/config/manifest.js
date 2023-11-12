75b8e5b6eaddea91e4f2123a64eac1a44f9e90b7
debicky <debickiart@gmail.com>
2023-11-12 22:04:33 +0100

    TASK-5 remove manifest link

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,4 @@
 //= link_directory ../stylesheets .css
-
-//= link_tree ../../../vendor/javascript .js
 //= link ../builds/tailwind.css
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
