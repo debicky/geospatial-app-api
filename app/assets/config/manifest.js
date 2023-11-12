0c4b21d3816ea32512bf15473ee6f2573e7f47db
debicky <debickiart@gmail.com>
2023-11-12 22:23:05 +0100

    TASK-5 import tailwind csses

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
+//= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link_tree ../builds
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
