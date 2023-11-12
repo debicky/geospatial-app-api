568380f9f2c6094b1ef516aa3cd39eccce4146e2
debicky <debickiart@gmail.com>
2023-11-12 22:24:43 +0100

    TASK-5 move tailwind.css to stylesheets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../builds
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
